594b6422c5f32bf95b92dd14273c456391f592d2
LenWebel <len.webel@purple.telstra.com>
2023-08-17 21:58:59 +0800

    add remote ip websocket

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
 const wordListElement = document.getElementById('word-list');
-const socket = new WebSocket('ws://go-web-app:8765' + window.location.search);
+const socket = new WebSocket('ws://192.168.86.22:8765' + window.location.search);
 const words = [];
 
 console.log('Attempting Connection...');
